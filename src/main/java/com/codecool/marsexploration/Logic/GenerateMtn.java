a8f1d930df32b0899f073f6d6f483ff04137c49c
BalintGeri <109275872+BalintGeri@users.noreply.github.com>
2023-02-01 14:00:36 +0100

    refactored gen mtn interface

--- src/main/java/com/codecool/marsexploration/Logic/GenerateMtn.java
+++ src/main/java/com/codecool/marsexploration/Logic/GenerateMtn.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Random;
 
 
-public class GenerateMtn implements EmptyProvider, ShapeProvider {
+public class GenerateMtn implements EmptyProvider {
     MapConfig mapconfig;
     Map map;
 
